b0932042a20fd30b43c55ac59676a2c7acdcc71f
Ross Mackay <ross@mcky.me>
2024-07-26 18:59:52 +0100

    test: add tests for read_notes

--- src/notes.rs
+++ src/notes.rs
@@ -1,7 +1,7 @@
-use std::{collections::HashMap, ffi::OsStr};
+use std::{ffi::OsStr, fs};
 use walkdir::WalkDir;
 
-use crate::Records;
+use crate::{Record, Records};
 
 pub fn read_notes(path: &str) -> anyhow::Result<Records> {
     let mut records: Records = Vec::new();
@@ -13,9 +13,9 @@ pub fn read_notes(path: &str) -> anyhow::Result<Records> {
         .filter(|entry| entry.path().extension().and_then(OsStr::to_str) == Some("md"))
     {
         if entry.file_type().is_file() {
-            let file_contents = std::fs::read_to_string(entry.path())?;
+            let file_contents = fs::read_to_string(entry.path())?;
 
-            let mut record: HashMap<String, String> = HashMap::new();
+            let mut record: Record = Record::new();
 
             record.insert("file_path".to_string(), entry.path().display().to_string());
             record.insert("file_contents".to_string(), file_contents);
@@ -25,4 +25,52 @@ pub fn read_notes(path: &str) -> anyhow::Result<Records> {
     }
 
     Ok(records)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Record;
+
+    use super::*;
+
+    use anyhow::Context;
+    use assert_fs::prelude::*;
+    use assert_fs::TempDir;
+    use maplit::hashmap;
+
+    #[test]
+    fn read_notes_returns_note_records_from_dir() {
+        let dir = TempDir::new().expect("failed to create new TempDir");
+
+        let first_note: Record = hashmap! {
+            "file_path".to_string() => dir.join("first-note.md").to_str().to_owned().unwrap().to_string(),
+            "file_contents".to_string() => "The contents of the file named first-note.md".to_string(),
+        };
+
+        let second_note: Record = hashmap! {
+            "file_path".to_string() => dir.join("second-note.md").to_str().to_owned().unwrap().to_string(),
+            "file_contents".to_string() => "The contents of the file named second-note.md".to_string(),
+        };
+
+        dir.child("first-note.md")
+            .write_str(&first_note["file_contents"])
+            .expect("failed to create file in TempDir");
+
+        dir.child("second-note.md")
+            .write_str(&second_note["file_contents"])
+            .expect("failed to create file in TempDir");
+
+        let path = dir.path().to_str().unwrap();
+        let notes = read_notes(path).expect("couldn't read notes");
+
+        assert!(
+            notes.contains(&first_note),
+            "expected {notes:?} to contain {first_note:?}"
+        );
+
+        assert!(
+            notes.contains(&second_note),
+            "expected {notes:?} to contain {second_note:?}"
+        );
+    }
 }
